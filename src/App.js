8f5d5a88603a279ae3fd8a58bd1fb24f064431f9
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-20 17:12:22 -0600

    Project complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+constructor() {
+  super()
+
+  this.state = {
+    isMobileOpen: false
+  }
+}
+
+toggleMenuSelect = () => {
+  this.setState({
+    isMobileOpen: !this.state.isMobileOpen
+  });
+}
+
+  render() {
+    return (
+      <div className="page-top">
+        
+        <nav className="navbar-container">
+          <div className="navbar">
+            <a className="navbar-logo-title" href=".page-top">
+              <img src="https://blackrockdigital.github.io/startbootstrap-agency/assets/img/navbar-logo.svg" alt="logo to site"></img>
+            </a>
+            <div className="navbar-selection-container">
+                <ul className="navbar-selection">
+                  <li className="nav-item">Services</li>
+                  <li className="nav-item">Portfolio</li>
+                  <li className="nav-item">About</li>
+                  <li className="nav-item">Team</li>
+                  <li className="nav-item">Contact</li>
+                </ul>
+            </div>
+
+
+            <button className="button-appear" onClick={this.toggleMenuSelect}>Menu <img src="https://www.contentformula.com/blog/wp-content/uploads/2016/06/hamburger-menu.png" alt="hamburger icon for drop down me" ></img></button>
+            <div className="mobile-navbar-selection-container">
+                <ul className={`mobile-navbar-selection ${this.state.isMobileOpen ? 'mobile-nav-item-open' : null}`}>
+                  <li className="mobile-nav-item">Services</li>
+                  <li className="mobile-nav-item">Portfolio</li> 
+                  <li className="mobile-nav-item">About</li>
+                  <li className="mobile-nav-item">Team</li>
+                  <li className="mobile-nav-item">Contact</li>
+                </ul>
+            </div>
+
+          </div>
+        </nav>
+
+
+        <header className="main-welcome-container">
+          <div className="main-welcome">
+            <p className="welcome-subhead">Welcome to our Studio!</p>
+            <div className="nice-header">It's Nice to Meet You</div>
+            <button className="more-button">Tell Me More</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
